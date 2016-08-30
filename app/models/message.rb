f8fe2e1ff52e075f55bf637430103fa2f5de7356
SBlazhko <blaghco@ukr.net>
2016-08-30 11:40:48 +0300

    ss

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,17 @@
 class Message < ApplicationRecord
+	belongs_to :chat_room
+	belongs_to :profile, foreign_key: 'sender_id'
 
-	belongs_to :sender, class_name: 'Profile', foreign_key: :sender_id
-	belongs_to :receiver, class_name: 'Profile', foreign_key: :receiver_id
+	validates :text, length: { minimum: 2, maximum: 100 }
+
+	def message_show_params
+		{
+			id: id,
+			chat_room_id: chat_room_id,
+			sender_id: sender_id,
+			text: text,
+			created_at: created_at
+		}
+	end
+
 end
